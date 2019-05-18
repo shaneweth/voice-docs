6ced19fb92f869447da6a68f9d12df057a7894cf
willow <shaneweth77@gmail.com>
2019-05-18 17:08:38 -0400

    restore

--- staging/js/logic.js
+++ staging/js/logic.js
@@ -1,26 +1,8 @@
-var sound = document.getElementById("myAudio");
-var playAction = document.getElementById("playButton");
-
-var isPlaying = false;
-
-function playAudio() {
-    sound.play();
-    isPlaying = true;
-    playAction.src = "https://findicons.com/files/icons/1676/primo/128/button_blue_pause.png";
-}
-
-function pauseAudio() {
-    sound.pause();
-    isPlaying = false;
-    playAction.src = "https://findicons.com/files/icons/1676/primo/128/button_blue_play.png";
-}
-
-function handleAudio() {
-    if (isPlaying == true) {
-        pauseAudio();
-    } else {
-        playAudio();
-    }
-}
-
-
+$('input[type="file"]').on('change', function () {
+    var reader = new FileReader();
+    reader.onload = function () {
+        var thisImage = reader.result;
+        localStorage.setItem("imgData", thisImage);
+    };
+    reader.readAsDataURL(this.files[0]);
+});
