a227cd6cae511c38f818de4af4754ea08ec51288
willow <shaneweth77@gmail.com>
2019-05-19 22:41:15 -0400

    setting up route for uploads

--- routes/projectApiRoutes.js
+++ routes/projectApiRoutes.js
@@ -30,24 +30,8 @@ module.exports = function (app) {
         });
     });
 
-    app.get('/', function (req, res) {
-        res.sendFile(__dirname + '/');
-    });
-
-    app.post('/', function (req, res) {
-        var form = new formidable.IncomingForm();
-
-        form.parse(req);
-
-        form.on('fileBegin', function (name, file) {
-            file.path = __dirname + '/uploads/' + file.name;
-        });
-
-        form.on('file', function (name, file) {
-            console.log('Uploaded ' + file.name);
-        });
-
-        res.sendFile(__dirname + '/');
+    app.get("/", function(req, res){
+        res.json(__dirname + "/uploads");
     });
 
     // app.post("/api/projects", function (req, res) {
