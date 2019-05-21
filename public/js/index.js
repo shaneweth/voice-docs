de85feea9015d296117c5ac52657235a64e323d5
willow <shaneweth77@gmail.com>
2019-05-21 12:13:51 -0400

    added modal for upload. Need to check the path route after upload

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,29 @@
-var 
+// For Modals
+
+var btn = document.getElementById("modal_opener");
+var modal = document.querySelector(".modal");
+
+function attachModalListeners(modalElem) {
+    modalElem.querySelector(".close_modal").addEventListener("click", toggleModal);
+    modalElem.querySelector(".overlay").addEventListener("click", toggleModal);
+}
+
+function detachModalListeners(modalElem) {
+    modalElem.querySelector(".close_modal").removeEventListener("click", toggleModal);
+    modalElem.querySelector(".overlay").removeEventListener("click", toggleModal);
+}
+
+function toggleModal() {
+    var currentState = modal.style.display;
+
+    // if modal is visible then hide, else display
+    if (currentState === "none") {
+        modal.style.display = "block";
+        attachModalListeners(modal);
+    } else {
+        modal.style.display = "none";
+        detachModalListeners(modal);
+    }
+}
+
+btn.addEventListener("click", toggleModal);
