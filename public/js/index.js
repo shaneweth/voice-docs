5a74a004538aa38e6309d569728e654825988ce5
willow <shaneweth77@gmail.com>
2019-05-21 17:13:24 -0400

    added Auth modal

--- public/js/index.js
+++ public/js/index.js
@@ -3,8 +3,23 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
+var collAuth = document.getElementsByClassName("collapsibleAuth");
+var j;
+
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+for (j = 0; j < collAuth.length; j++) {
+  collAuth[j].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
