5d49217b9a445f22744bf055c9cf14a505e39697
logan hill <loganhill@logans-MacBook-Pro.local>
2019-05-23 15:09:30 -0400

    finished the data loop

--- public/js/index.js
+++ public/js/index.js
@@ -1,149 +1,114 @@
 
-// // for hidden collapsible upload element
+// for hidden collapsible upload element
 
-// var coll = document.getElementsByClassName("collapsible");
-// var i;
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-// var collAuth = document.getElementsByClassName("collapsibleAuth");
-// var j;
+var collAuth = document.getElementsByClassName("collapsibleAuth");
+var j;
 
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function () {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.display === "block") {
-//       content.style.display = "none";
-//     } else {
-//       content.style.display = "block";
-//     }
-//   });
-// }
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
 
-// for (j = 0; j < collAuth.length; j++) {
-//   collAuth[j].addEventListener("click", function () {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.display === "grid") {
-//       content.style.display = "none";
-//     } else {
-//       content.style.display = "grid";
-//     }
-//   });
-// }
+for (j = 0; j < collAuth.length; j++) {
+  collAuth[j].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "grid") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "grid";
+    }
+  });
+}
 
-// //AUDIO PLAYER
-// // Elements
+//AUDIO PLAYER
+// Elements
 
-// const player = document.querySelector(".player");
-// const audio = player.querySelector(".viewer");
-// const progress = player.querySelector(".progress");
-// const progressBar = player.querySelector(".progress_filled");
-// const toggle = player.querySelector(".toggle");
-// const skipButtons = player.querySelectorAll("[data-skip]");
-// const ranges = player.querySelectorAll(".player_slider");
+const player = document.querySelector(".player");
+const audio = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress_filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player_slider");
 
 
-// // functions
+// functions
 
-// function togglePlay() {
+function togglePlay() {
 
-//   // ternary op instead of if/else
-//   const method = audio.paused ? "play" : "pause";
-//   audio[method]();
-// }
+  // ternary op instead of if/else
+  const method = audio.paused ? "play" : "pause";
+  audio[method]();
+}
 
-// function updateButton() {
-//   const icon = this.paused ? "►" : "❚❚";
-//   console.log(icon);
-//   toggle.textContent = icon;
-// }
+function updateButton() {
+  const icon = this.paused ? "►" : "❚❚";
+  console.log(icon);
+  toggle.textContent = icon;
+}
 
-// // Skip Function
+// Skip Function
 
-// function skip() {
-//   console.log(this.dataset.skip);
-//   audio.currentTime += parseFloat(this.dataset.skip);
-// }
+function skip() {
+  console.log(this.dataset.skip);
+  audio.currentTime += parseFloat(this.dataset.skip);
+}
 
-// function handleRangeUpdate() {
-//   audio[this.name] = this.value;
-//   console.log(this.name);
-//   console.log(this.value);
-// }
+function handleRangeUpdate() {
+  audio[this.name] = this.value;
+  console.log(this.name);
+  console.log(this.value);
+}
 
-// function handleProgress() {
-//   // updates the flex-basis of .progress_filled
-//   const percent = (audio.currentTime / audio.duration) * 100;
-//   progressBar.style.flexBasis = `${percent}%`;
-// }
+function handleProgress() {
+  // updates the flex-basis of .progress_filled
+  const percent = (audio.currentTime / audio.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
 
-// function scrub(e) {
-//   const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
-//   audio.currentTime = scrubTime;
-//   console.log(e);
-// }
-// // event listeners
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  audio.currentTime = scrubTime;
+  console.log(e);
+}
+// event listeners
 
-// audio.addEventListener("click", togglePlay);
-// audio.addEventListener("play", updateButton);
-// audio.addEventListener("pause", updateButton);
+audio.addEventListener("click", togglePlay);
+audio.addEventListener("play", updateButton);
+audio.addEventListener("pause", updateButton);
 
-// // listens for timeupdate for the handleProgress fn
-// audio.addEventListener("timeupdate", handleProgress);
+// listens for timeupdate for the handleProgress fn
+audio.addEventListener("timeupdate", handleProgress);
 
-// toggle.addEventListener("click", togglePlay);
+toggle.addEventListener("click", togglePlay);
 
-// skipButtons.forEach(button => button.addEventListener("click", skip));
+skipButtons.forEach(button => button.addEventListener("click", skip));
 
-// ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
-// ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
 
-// let mousedown = false;
-// progress.addEventListener("click", scrub);
-// progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
-// progress.addEventListener("mousedown", () => mousedown = true);
-// progress.addEventListener("mouseup", () => mousedown = false);
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => mousedown = true);
+progress.addEventListener("mouseup", () => mousedown = false);
 
 $(".project-option").on("click", function (e) {
-  window.AudioContext = window.AudioContext || window.webkitAudioContext;
   let text = $(this).text();
   console.log(text);
   $.get("api/projects/" + text, function (data) {
+    $(".player_video").attr("src", data.mainFile);
     console.log(data);
   });
-
-  var context = new AudioContext();
-  var source;
-  // var spinner = document.getElementById('spinner');
-  // spinner.hide = function () {
-  //   this.style.display = 'none';
-  // };
-  // spinner.show = function () {
-  //   this.style.display = 'block';
-  // }
-  function stopTunes() {
-    if (source.stop) {
-      source.stop();
-    }
-  }
-  function playTunes() {
-    var request = new XMLHttpRequest();
-    request.open("GET", "http://localhost:3000/audio/", true);
-    request.responseType = "arraybuffer";
-    spinner.show();
-    request.onload = function () {
-      spinner.hide();
-      var Data = request.response;
-      process(Data);
-    };
-    request.send();
-  }
-  function process(Data) {
-    source = context.createBufferSource(); // Create Sound Source
-    context.decodeAudioData(Data, function (buffer) {
-      source.buffer = buffer;
-      source.connect(context.destination);
-      source.start(context.currentTime);
-    });
-  }
 })
--- routes/projectApiRoutes.js
+++ routes/projectApiRoutes.js
@@ -1,7 +1,6 @@
 require("express-fileupload");
 const db = require("../models");
 const aws = require('aws-sdk');
-const s3Stream = require('s3');
 
 aws.config.update({
     accessKeyId: "AKIAIYSEHHRUPS64F53A",
@@ -33,30 +32,18 @@ module.exports = function (app) {
             include: [db.User]
         }).then(function (dbProject) {
 
-            let params = {
-                Bucket: "teamawesome123",
-                Key: dbProject.oName + "/" + dbProject.title + "/" + dbProject.mainFile,
-            };
+            // let params = {
+            //     Bucket: "teamawesome123",
+            //     Key: dbProject.oName + "/" + dbProject.title + "/" + dbProject.mainFile,
+            // };
 
-            let client = s3Stream.createClient({
-                accessKeyId: "AKIAIYSEHHRUPS64F53A",
-                secretAccessKey: "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"
-            })
-
-            let dl = client.downloadStream(params);
-            dl.on('error', function() {
-                res.status(404).send(params.Key + ' not found in ' + params.Bucket);
-            });
-            dl.on('httpHeaders', function(status, headers, data) {
-                res.json(data);
-            })
-
-            // var fs = s3.getObject(params).createReadStream();
+            // res.attachment(params.Key);
+            // let fs = s3.getObject(params).createReadStream();
             // fs.pipe(res);
 
             // console.log(res);
 
-            //res.json(dbProject);
+            res.json(dbProject);
         });
     });
 
@@ -79,21 +66,20 @@ module.exports = function (app) {
             if (err) throw err;
             if (data) {
                 console.log(data);
+                let newProject = {
+                    title: req.body.title,
+                    description: req.body.description,
+                    category: req.body.category,
+                    location: req.body.location,
+                    oName: req.body.oName,
+                    UserUsername: req.body.oName,
+                    mainFile: data.Location,
+                }
+
+                db.Project.create(newProject).then(function (dbProject) {
+                    res.json(dbProject);
+                });
             }
-        })
-
-        let newProject = {
-            title: req.body.title,
-            description: req.body.description,
-            category: req.body.category,
-            location: req.body.location,
-            oName: req.body.oName,
-            UserUsername: req.body.oName,
-            mainFile: file.originalname,
-        }
-
-        db.Project.create(newProject).then(function (dbProject) {
-            res.json(dbProject);
         });
     });
 
