177e4d14792cabef060e7cf7d42ed8534364f5f5
willow <shaneweth77@gmail.com>
2019-05-22 21:26:15 -0400

    changes to layout

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 // for hidden collapsible upload element
 
 var coll = document.getElementsByClassName("collapsible");
@@ -29,3 +30,75 @@ for (j = 0; j < collAuth.length; j++) {
     }
   });
 }
+
+// Elements
+
+const player = document.querySelector(".player");
+const audio = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress_filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player_slider");
+
+
+// functions
+
+function togglePlay() {
+
+    // ternary op instead of if/else
+    const method = audio.paused ? "play" : "pause";
+    audio[method]();
+}
+
+function updateButton() {
+    const icon = this.paused ? "►" : "❚❚";
+    console.log(icon);
+    toggle.textContent = icon;
+}
+
+// Skip Function
+
+function skip() {
+    console.log(this.dataset.skip);
+    audio.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    audio[this.name] = this.value;
+    console.log(this.name);
+    console.log(this.value);
+}
+
+function handleProgress() {
+    // updates the flex-basis of .progress_filled
+    const percent = (audio.currentTime / audio.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    audio.currentTime = scrubTime;
+    console.log(e);
+}
+// event listeners
+
+audio.addEventListener("click", togglePlay);
+audio.addEventListener("play", updateButton);
+audio.addEventListener("pause", updateButton);
+
+// listens for timeupdate for the handleProgress fn
+audio.addEventListener("timeupdate", handleProgress);
+
+toggle.addEventListener("click", togglePlay);
+
+skipButtons.forEach(button => button.addEventListener("click", skip));
+
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => mousedown = true);
+progress.addEventListener("mouseup", () => mousedown = false);
