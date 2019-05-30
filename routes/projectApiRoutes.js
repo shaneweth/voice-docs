f38c2f8f565d5497f2b369054f42e698a7730612
unknown <mstevens9335@yahoo.com>
2019-05-30 14:49:12 -0400

    argh

--- routes/projectApiRoutes.js
+++ routes/projectApiRoutes.js
@@ -1,18 +1,16 @@
 require("express-fileupload");
 const db = require("../models");
 const aws = require('aws-sdk');
-const dotenv = require('dotenv');
 
-dotenv.config();
+// Node Dependencies
+const express = require('express');
+const router = express.Router();
+// testing
 
-// aws.config.update({
-//     accessKeyId: process.env.API_KEY,
-//     secretAccessKey: process.env.SECRET_KEY
-// });
-aws.config.update({
+let s3 = new aws.S3({
     accessKeyId: process.env.accessKeyId,
     secretAccessKey: process.env.secretAccessKey
-});
+  });
 
 module.exports = function (app) {
     app.get("/api/projects", function (req, res) {
@@ -28,6 +26,24 @@ module.exports = function (app) {
             res.json(dbProject);
         });
     });
+
+
+// Index Redirect
+router.get('/', function (req, res) {
+    res.redirect('/');
+  });
+  
+  
+  // Index Page (render all burgers)
+  router.get('/index', function (req, res) {
+    project.selectAll(function (data) {
+      var hbsObject = { projects: data };
+      //console.log(hbsObject);
+      res.render('index', hbsObject);
+    });
+  });
+
+    // ------------------------
 
     app.get("/api/projects/:title/:oName", function (req, res) {
         const s3 = new aws.S3();
@@ -46,7 +62,7 @@ module.exports = function (app) {
     app.post("/api/projects", function (req, res) {
         const s3 = new aws.S3();
 
-        console.log(req.files[0]);
+        // console.log(req.files[0]);
 
         let file = req.files[0];
         let buffer = file.buffer;
@@ -71,26 +87,6 @@ module.exports = function (app) {
                     Username: req.body.oName,
                     mainFile: data.Location,
                 }
-
-                db.User.findOne({
-                    where: {
-                        username: newProject.oName,
-                    }
-                }).then(function(dbUser) {
-                    let projects = dbUser.pNames;
-                    console.log("\n\n\n" + projects + "\n\n\n");
-                    if(projects === "null" || projects === null)
-                        projects = newProject.title + ",";
-                    else
-                        projects += newProject.title + ",";
-
-                    db.User.update(
-                        {pNames: projects},
-                        {returning: true, where: {username: newProject.oName}}
-                    ).then(function(data) {
-                        console.log(data);
-                    });
-                })
 
                 db.Project.create(newProject).then(function (dbProject) {
                     res.json(dbProject);
